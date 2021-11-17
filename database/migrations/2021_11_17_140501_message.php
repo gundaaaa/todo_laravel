3d82632d7ba411707802aea67162665db0129e8c
gundaaaa <83895029+gundaaaa@users.noreply.github.com>
2021-11-17 23:10:41 +0900

    migrations message追加

---
+++ database/migrations/2021_11_17_140501_message.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Message extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->charset("utf8");
+            $table->text('message');
+            $table->date('time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
